6e2f94e22902699ab37deddda20cd66faf1634ae
Gabriel Costanza <costanzag1999@gmail.com>
2022-04-23 17:21:54 +0200

    checkpoint

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -9,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          let products = JSON.parse(xhr.responseText);
+          alert(products.mierda);
+        }
+      }
+    };
+    xhr.open("POST", "http://localhost:8080/bestiario_war_exploded/hello-servlet", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send();
+
+  }
 }
