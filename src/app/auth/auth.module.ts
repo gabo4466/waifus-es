3e8ab505ce62d8d088ce75f28daae78bef733d30
Gabriel Costanza <costanzag1999@gmail.com>
2022-04-24 21:55:00 +0200

    RegisterPage

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
+import {SharedModule} from "../shared/shared.module";
 
 
 
@@ -17,6 +18,7 @@ import {RegisterComponent} from "./register/register.component";
   ],
   imports: [
     CommonModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
