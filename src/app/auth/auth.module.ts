2cd93cb822ae425f4af0b4bec2930a136cc5b2ec
Gabriel Costanza <costanzag1999@gmail.com>
2022-04-25 20:34:16 +0200

    Registro tests

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AppRoutingModule} from "../app-routing.module";
+import {FormsModule} from "@angular/forms";
 
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
@@ -22,6 +23,7 @@ import { AuthComponent } from './auth.component';
   imports: [
     CommonModule,
     AppRoutingModule,
+    FormsModule
   ]
 })
 export class AuthModule { }
