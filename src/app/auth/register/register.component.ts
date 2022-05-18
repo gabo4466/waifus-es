d3af935f256c7fbfa4f2b30fe1fc754da254918d
Jaimercal <romerocal.jaime@gmail.com>
2022-05-18 18:47:32 +0200

    Registro user

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {UserModel} from "../../model/user.model";
+import {Constants} from "../../common/constants";
+import {formatDate} from "@angular/common";
 
 @Component({
   selector: 'app-register',
@@ -7,14 +11,35 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  private url = Constants.apiURL;
+  public user:UserModel;
+  constructor( private http: HttpClient ) {
+    this.user = new UserModel();
+    this.url += "register";
+  }
 
   ngOnInit(): void {
   }
 
+  formatDateee(date:Date) {
+    var d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+    if (month.length < 2)
+      month = '0' + month;
+    if (day.length < 2)
+      day = '0' + day;
+
+    return [year, month, day].join('');
+  }
+
   registerUser(form : NgForm){
-    console.log(form);
+    console.log(this.formatDateee(this.user._birthday));
+    this.http.post<Object>(this.url, JSON.stringify(this.user).replace(/[/_/]/g, '')).subscribe( (resp:any) => {
+      console.log(resp);
+    } );
   }
 
 }
--- src/app/model/user.model.ts
+++ src/app/model/user.model.ts
@@ -1,9 +1,21 @@
 export class UserModel {
   public _email:String;
   public _password:String;
+  public _nickname:String;
+  public _name:String;
+  public _pass:String;
+  public _birthday:Date;
+  public _adultContent:boolean;
+  public _terms:boolean;
 
   constructor() {
     this._email = "";
     this._password = "";
+    this._nickname = "";
+    this._name = "";
+    this._pass = "";
+    this._birthday = new Date(0);
+    this._adultContent = false;
+    this._terms = false;
   }
 }
